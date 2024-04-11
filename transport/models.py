5d02607a39d7b77fc94f0f530b823651fcbf9d28
Guilherme Costa <guilherme.partic@gmail.com>
2024-04-11 16:57:48 -0300

    created the app

---
+++ transport/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+
+class Transport(models.Model):
+    name = models.CharField(max_length=255, blank=False, null=False)
+
+    def __str__(self) -> str:
+        return self.name
