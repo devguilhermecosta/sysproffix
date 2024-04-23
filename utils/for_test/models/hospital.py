f38d0a9157fa5d93197cdd4c3ed9ca1e34211976
Guilherme Costa <guilherme.partic@gmail.com>
2024-04-23 17:53:12 -0300

    update: add the new parameters

--- utils/for_test/models/hospital.py
+++ utils/for_test/models/hospital.py
@@ -1,11 +1,17 @@
 from hospital.models import Hospital
 
 
-def create_hospital(name: str = 'Hospital') -> Hospital:
+def create_hospital(name: str = 'Hospital',
+                    city: str = 'São Paulo',
+                    state: str = 'SP') -> Hospital:
     """
         Create a new instance of Hospital model.
     """
-    new_hospital = Hospital.objects.create(name=name)
+    new_hospital = Hospital.objects.create(
+        name=name,
+        city=city,
+        state=state,
+    )
     new_hospital.save()
 
     return new_hospital
