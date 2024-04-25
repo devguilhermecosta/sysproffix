3f8d00ec6a3650202e08913ae2d7d0c6678a1124
Guilherme Costa <guilherme.partic@gmail.com>
2024-04-25 14:37:11 -0300

    feat: add the myaccount url

--- user/urls.py
+++ user/urls.py
@@ -9,4 +9,8 @@ urlpatterns = [
     path('<int:id>/details/', views.UserDetailView.as_view(), name='details'),
     path('<int:id>/delete/', views.UserDeleteView.as_view(), name='delete'),
     path('minha-conta/', views.MyAccountView.as_view(), name='account'),
+    path('minha-conta/change-password/',
+         views.MyAccountView.as_view(),
+         name='change-password',
+         ),
 ]
